a8989be15af7c0077354a47407977333476b516a
Hang Zhou <1174714790@qq.com>
2024-04-29 12:37:30 +0800

    [dev]: 配置 webpack 警告的阈值

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    performance: {
+      hints:'warning',
+      //入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      //生成文件的最大体积
+      maxAssetSize: 30000000,
+      //只给出 js 文件的性能提示
+      assetFilter: function(assetFilename) {
+        return assetFilename.endsWith('.js');
+      }
+    }
+  }
 });
