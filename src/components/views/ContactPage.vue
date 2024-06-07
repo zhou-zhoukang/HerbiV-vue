38f46b0ca12d11c7f8e0023752335c4d78953a3d
Hang Zhou <1174714790@qq.com>
2024-06-07 22:50:27 +0800

    [docs]: 在 package.json 中添加基本信息

--- src/components/views/ContactPage.vue
+++ src/components/views/ContactPage.vue
@@ -9,7 +9,7 @@ import {Compass, Message} from "@element-plus/icons-vue";
     <el-icon size="40"><Message /></el-icon>
     <p>xxx：xxx@qq.com</p>
     <p>xxx：xxx@qq.com</p>
-    <p>WebApp：hangzhou@njfu.edu.com</p>
+    <p>WebApp：hangzhou@njfu.edu.cn</p>
     <br>
     <el-icon size="40"><Compass /></el-icon>
     <p>南京中医药大学 Nanjing University of Chinese Medicine.</p>
